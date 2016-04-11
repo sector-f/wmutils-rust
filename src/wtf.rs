1d0fbcfb6761490794ed9abb1864689b04d17ef4
panicbit <panicbit.dev@gmail.com>
2016-04-11 22:53:08 +0200

    Use clap in wtf

--- src/wtf.rs
+++ src/wtf.rs
@@ -1,27 +1,22 @@
 extern crate xcb;
+extern crate clap;
 
-use std::env;
-use std::process;
-use xcb::xproto;
+use clap::{App, Arg};
 
 pub mod util;
 
-fn usage(programname: &String) {
-    println!("Usage: {} <wid>", programname);
-    process::exit(1);
-}
-
 fn main() {
-    let programname = env::args().nth(0).unwrap_or_else(|| String::new());
-    let args: Vec<_> = env::args().collect();
-    if args.len() != 2 || args[1] == "-h" || args[1] == "--help" {
-        usage(&programname);
-    }
+    let args = App::new("wtf")
+        .about("transfer window focus")
+        .arg(Arg::with_name("wid").required(true))
+        .get_matches();
 
-    let connection = util::init_xcb(&programname);
+    let wid = args.value_of("wid").unwrap(); // Unwrap is fine, the arg is required
+    let wid = util::get_window_id(wid);
 
-    let win = util::get_window_id(&args[1]);
+    let connection = util::init_xcb("wtf");
 
-    xproto::set_input_focus(&connection, xproto::INPUT_FOCUS_POINTER_ROOT as u8, win, xproto::TIME_CURRENT_TIME);
+    xcb::set_input_focus(&connection, xcb::INPUT_FOCUS_POINTER_ROOT as u8, wid, xcb::TIME_CURRENT_TIME);
+
     connection.flush();
 }
